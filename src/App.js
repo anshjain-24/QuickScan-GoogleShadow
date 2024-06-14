45cc535676025ac89798dc934ee36506032e936a
Ansh Jain <ansh.jain@netweb.biz>
2024-06-14 18:05:57 +0530

    just to save in remote

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React,{useEffect, useState} from 'react'
+
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Routes from './components/Routes.jsx'
 
 const App = () => {
+  
+  const [dark, setDark] = useState(false)
+  console.log("dark : ",dark);
   return (
-    <div>
-        <h1  className="text-4xl p-10 font-bold underline bg-gray-500">
-             App 
+    <div className={dark ? 'dark' : ''}>
+        <h1  className="text-4xl p-10 font-bold underline text-gray-100 bg-gray-200 dark:bg-gray-800 dark:text-gray-500 min-h-screen">
+             <Navbar />
+             <Routes />
+             <Footer />
         </h1>
     </div>
   )
--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import ReactDOM from 'react-dom'
 import App from './App';
 import './global.css'
 
-ReactDOM.render(<App/>, document.getElementById("root")); 
+ReactDOM.render(
+    <>
+    <App/>
+    </>, document.getElementById("root")); 
